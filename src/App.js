b23216f0c22a46defa83d32bcc1771f3cdfab7af
Rohan Nayak <rohannayak2071@gmail.com>
2022-06-25 11:20:41 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+import { Container} from 'react-bootstrap';
+
 function App() {
+  const[inputFields,setInputFields] = useState([
+    {firstName : '',lastName : ''}
+  ])
+
+  const handleChangeInput = (index , e) =>{
+    console.log(index,e.target.name) // gives output 0 , firstName
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <h1>Add New Member</h1>
+          <form>
+            {inputFields.map((inputField,index)=>(
+              <div key={index}>
+                <label>First Name : </label>
+                <input name='firstName' type='text' placeholder='firstName' value={inputField.firstName} onChange={e => handleChangeInput(index,e)}></input>
+                <label>Last Name : </label>
+                <input name='lastName' type='text' placeholder='lastName' value={inputField.lastName} onChange={e => handleChangeInput(index,e)}></input>
+                <button type='button'>Add</button>
+                <button type='button'>Remove</button>
+              </div>
+            ))}
+            <button type='button'>Send !</button>
+          </form>
+        </Container>
     </div>
   );
 }
